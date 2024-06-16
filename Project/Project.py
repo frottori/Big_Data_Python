8506357a2d2a9474d85ccbc5380d15503fc2e403
Frosso Varsou <frossovarsou@gmail.com>
2024-06-17 02:10:59 +0300

    Update: Added comments

--- Project/Project.py
+++ Project/Project.py
@@ -8,20 +8,21 @@ from mlxtend.frequent_patterns import apriori, association_rules
 '''
 ASSOCIATION RULES
 '''
-
+# Generate association rules from frequent itemsets
 def assoc_rules(data, m_sup, m_thr):
-
+    
+    # Transaction Encoder to convert data into a one-hot (n-dimension) encoded NumPy array
     te = TransactionEncoder()
     te_data = te.fit(data).transform(data)
     pr = pd.DataFrame(te_data,columns=te.columns_)
 
-    # Apriori function to extract frequent itemsets for association rule mining
-    # Support threshold can be mentioned to retrieve frequent itemset
+    # Apriori function: to extract frequent itemsets for association rule mining
+    # min_support: Minimum support threshold for the frequent itemsets
     freq_items = apriori(pr, min_support = m_sup, use_colnames = True, verbose = 1)
 
     # Association rule mining
-    #Let's view our interpretation values using the Association rule function.
-    #Function to generate association rules from frequent itemsets
+    # Function to generate association rules from frequent itemsets
+    # min_threshold: Minimum threshold for the association rules
     pr_ar = association_rules(freq_items, metric = "confidence", min_threshold = m_thr)
     return freq_items, pr_ar
 
@@ -114,15 +115,18 @@ def kmeans(dset, k=2, tol=1e-4):
 '''MY FUNCTIONS'''
 # Function to convert Indian Rupees to Euros and round to 2 decimal places
 def inr_to_euro(price_inr, exchange_rate=0.011):
-    price_inr = float(price_inr.replace('₹', '').replace(',', '').strip())
+    # Remove the currency symbol and commas and convert to float
+    price_inr = float(price_inr.replace('₹', '').replace(',', '').strip()) 
+    # Convert to Euros from Indian Rupees
     price_euro = price_inr * exchange_rate
+    # Round to 2 decimal places
     return round(price_euro, 2)
 
 # Function to extract the required information for processor column
 def extract_processor_info(processor):
-    parts = processor.split(',')  # Split the string by comma
+    parts = processor.split(',')  # Split the string by comma (processor, core, clock speed)
 
-    # Remove the word "Processor" if found in any part
+    # Remove the word "Processor" if found in any part 
     cleaned_parts = []
     for part in parts:
         cleaned_part = part.replace('Processor', '').strip()
@@ -144,13 +148,15 @@ def extract_processor_info(processor):
 
 # Function to extract the required information for ram column
 def extract_ram_info(ram):
-    parts = ram.split(',')  # Split the string by comma
+    parts = ram.split(',')  # Split the string by comma (ram, storage type)
+
+    # Remove the word "inbuilt" if found in any part
     cleaned_parts = []
     for part in parts:
         cleaned_part = part.replace('inbuilt', '')
         cleaned_parts.append(cleaned_part)
     parts = cleaned_parts
-        
+    
     ram_type = parts[0].strip()
     # if there is a second part, extract the storage type
     if len(parts) > 1:
@@ -160,51 +166,55 @@ def extract_ram_info(ram):
     return ram_type, storage_type
 
 def one_hot_enco_proc(df):
-    # Extract information into a list of tuples
+    # Extracting info into a list of tuples and appending to data
     data = []
     for p in df['processor']:
         info = extract_processor_info(p)
         data.append(info)
 
-    # Create DataFrame
+    # Create new dataframe with three processor columns
     df_proc = pd.DataFrame(data, columns=['processor_type', 'core_type', 'clock_speed'])
 
-    # Remove any additional spaces around the processor_type
+    # Stripping additional spaces around the processor type
     df_proc['processor_type'] = df_proc['processor_type'].str.strip().str.replace('  ', ' ')
 
-    # Add the extracted columns to the original DataFrame
+    # add the new cols to the original dataframe (df)
     df[['processor_type', 'core_type', 'clock_speed']] = df_proc
 
-    # Convert columns to integer codes 
-    df['processor_type_code'] = pd.factorize(df['processor_type'])[0] # [0]: Return the codes [1]: Return the unique values
+    # Convert columns to integer codes (One hot encoding - 1 dimension)
+    # pd.factorize() returns a tuple of two values
+    # [0]: Return the codes [1]: Return the unique values
+    # We only need the codes
+    df['processor_type_code'] = pd.factorize(df['processor_type'])[0] 
     df['core_type_code'] = pd.factorize(df['core_type'])[0]
     df['clock_speed_code'] = pd.factorize(df['clock_speed'])[0]
     return df
 
 def one_hot_enco_ram(df):
-    # Extract information into a list of tuples
+    # Extracting info into a list of tuples and appending to data
     data = []
     for p in df['ram']:
         info = extract_ram_info(p)
         data.append(info)
 
-    # Create DataFrame
+     # Create new dataframe with the two cols (ram_type, storage_type)
     df_ram = pd.DataFrame(data, columns=['ram_type', 'storage_type'])
 
-    # Remove any additional spaces around the ram_type
-    df_ram['ram_type'] = df_ram['ram_type'].str.strip().str.replace('  ', '')
-
-    # Add the extracted columns to the original DataFrame
+    # add the two new cols to the original dataframe (df)
     df[['ram_type','storage_type']] = df_ram
 
-    # Convert columns to integer codes 
-    df['ram_type_code'] = pd.factorize(df['ram_type'])[0] # [0]: Return the codes [1]: Return the unique values
+    # Convert columns to integer codes (One hot encoding - 1 dimension)
+    # pd.factorize() returns a tuple of two values
+    # [0]: Return the codes [1]: Return the unique values
+    # We only need the codes 
+    df['ram_type_code'] = pd.factorize(df['ram_type'])[0] 
     df['storage_type_code'] = pd.factorize(df['storage_type'])[0]
     return df
 
 def price_processor_analysis(df):
-    # Scatter plot of the data points
+
     fig, ax = plt.subplots(figsize=(8, 6))
+    # Scatter plot of the data points (Processor (int) vs Price)
     plt.scatter(x=df['processor_type_code'], y=df['Price_Euro'], s=2)
     ax.set_xlabel(r'Processor', fontsize=14)
     ax.set_ylabel(r'Price', fontsize=14)
@@ -212,17 +222,19 @@ def price_processor_analysis(df):
     plt.yticks(fontsize=12)
     plt.show()
 
-    # Apply the k-means algorithm to the dataset
+    # Apply the k-means algorithm
     np.random.seed(42)  # Set seed to 42 for reproducibility
-    k = 4               # Number of clusters (for low range processors, medium range processors, and high range processors)
-    df['centroid'], df['error'], centroids =  kmeans(df[['processor_type_code','Price_Euro']], k)
+    k = 4               # Number of clusters (for low range phones, medium range phones, high range phones, and luxury phones)
+    df['centroid'], df['error'], centroids =  kmeans(df[['processor_type_code','Price_Euro']], k) # Apply k-means and get the centroids
 
-    # Colors for the clusters in the scatter plot
+    # Colors for the clusters/centrods in the scatter plot
     customcmap = ListedColormap(["crimson", "mediumblue", "darkmagenta", "forestgreen"])
-    fig, ax = plt.subplots(figsize=(8, 6))
+
+    # Plot the scatter plot for the data points now in different colour clusters (circle markers)
     plt.scatter(x=df['processor_type_code'], y=df['Price_Euro'],  marker = 'o', 
                 c=df['centroid'].astype('category'), 
                 cmap = customcmap, s=2, alpha=0.5)
+    # Plot the centroids in the same plot (square markers)
     plt.scatter(centroids.iloc[:,0], centroids.iloc[:,1],  
                 marker = 's', s=30, c=[0, 1, 2, 3], 
                 cmap = customcmap)
@@ -234,7 +246,7 @@ def price_processor_analysis(df):
     return df
 
 def price_ram_analysis(df):
-    # Scatter plot of the data points
+    # Scatter plot of the data points (Ram (int) vs Price)
     fig, ax = plt.subplots(figsize=(8, 6))
     plt.scatter(x=df['ram_type_code'], y=df['Price_Euro'], s=2)
     ax.set_xlabel(r'Ram', fontsize=14)
@@ -244,16 +256,18 @@ def price_ram_analysis(df):
     plt.show()
 
     # Apply the k-means algorithm to the dataset
-    np.random.seed(42)  # Set seed to 42 for reproducibility
-    k = 4               # Number of clusters (for low range ram, medium range ram, and high range ram)
+    np.random.seed(42) 
+    k = 4               # Number of clusters (for low range phones, medium range phones, high range phones, and luxury phones)
     df['centroid'], df['error'], centroids =  kmeans(df[['ram_type_code','Price_Euro']], k)
 
     # Colors for the clusters in the scatter plot
     customcmap = ListedColormap(["crimson", "mediumblue", "darkmagenta", "forestgreen"])
-    fig, ax = plt.subplots(figsize=(8, 6))
+
+    # Plot the scatter plot for the data points now in different colour clusters (circle markers)
     plt.scatter(x=df['ram_type_code'], y=df['Price_Euro'],  marker = 'o', 
                 c=df['centroid'].astype('category'), 
                 cmap = customcmap, s=2, alpha=0.5)
+    # Plot the centroids in the same plot (square markers)
     plt.scatter(centroids.iloc[:,0], centroids.iloc[:,1],  
                 marker = 's', s=30, c=[0, 1, 2, 3], 
                 cmap = customcmap)
@@ -265,23 +279,27 @@ def price_ram_analysis(df):
     return df
 
 def assoc_mining_proc(df, c, min_sup=0.2, min_thr=0.6):
+    # Filter the dataframe by the cluster
     df = df[df['centroid'] == c]
     # Dataframe with all processor characteristics
     pr = pd.DataFrame(df['processor']);
-    # Remove the thin space character
-    pr = pr.replace('\u2009', '', regex=True)
-    data = list(pr["processor"].apply(lambda x:x.split(",") ))
+    
+    pr = pr.replace('\u2009', '', regex=True) # Removes the thin space character from col processor
+    # Split the processor column by comma into lists
+    data = list(pr["processor"].apply(lambda x:x.split(","))) # spit the processor column by comma into lists
 
-    freq_items, pr_ar = assoc_rules(data, min_sup, min_thr)
+    # Association rules
+    freq_items, pr_ar = assoc_rules(data, min_sup, min_thr) 
     return freq_items, pr_ar
 
 def assoc_mining_ram(df, c, min_sup=0.2, min_thr=0.6):
+    # Filter the dataframe by the cluster
     df = df[df['centroid'] == c]
     # Dataframe with all ram characteristics
-    df.loc[:, 'ram'] = df['ram'].replace('inbuilt', '', regex=True)
     pr = pd.DataFrame(df['ram'])
+    # Split the processor column by comma into lists
     data = list(pr["ram"].apply(lambda x:x.split(",") ))
-
+    # Association rules
     freq_items, pr_ar = assoc_rules(data, min_sup, min_thr)
     return freq_items, pr_ar
 
@@ -293,56 +311,58 @@ if __name__ == "__main__":
     # Add a unique identifier for each row
     df['id'] = range(1, len(df) + 1)  
 
-    # Map the processor categories to integers using one-hot encoding (same processor type will have the same code)
+    # Map the processor col to int using one-hot encoding (same processor type will have the same code)
     df = one_hot_enco_proc(df)
 
-    #! 1. Cluster Analysis and Association Rules for Price and Processor_type
-    price_processor_analysis(df)
+    #! 1. Cluster Analysis and Association Rules for Price and Processor
+    # Clustering
+    df = price_processor_analysis(df)
 
-    #^ Low-end phones
+    # Association rules
+    # Low-end phones
     freq_items3, pr_ar3 = assoc_mining_proc(df, 3, 0.1, 0.1)
-    print(freq_items3.head())
-    print(pr_ar3.head())
+    print(freq_items3.head()) # frequent itemsets
+    print(pr_ar3.head()) # association rules
 
-    #^ Mid-range phones
+    # Mid-range phones
     freq_items0, pr_ar0 = assoc_mining_proc(df, 0, 0.1, 0.1)
     print(freq_items0.head())
     print(pr_ar0.head())
 
-    #^ High-end phones
+    # High-end phones
     freq_items1, pr_ar1 = assoc_mining_proc(df, 1, 0.1, 0.1)
     print(freq_items1.head())
     print(pr_ar1.head())
 
-    #^ Very high-end phones (Luxury phones)
+    # Luxury phones
     freq_items2, pr_ar2 = assoc_mining_proc(df, 2, 0.1, 0.1)
     print(freq_items2.head())
     print(pr_ar2.head()) 
 
-    
-    # Map the ran categories to integers using one-hot encoding (same ra type will have the same code)
+    # Map the ram col to int using one-hot encoding
     df = one_hot_enco_ram(df)
-    print(df.head())
 
-    #! 2. Cluster Analysis and Association Rules for Price and Ram_type
-    price_ram_analysis(df)
+    #! 2. Cluster Analysis and Association Rules for Price and Ram
+    # Clustering
+    df = price_ram_analysis(df)
 
-    #^ Low-end phones
+    # Association rules
+    # Low-end phones
     f_r3, ram_ar3 = assoc_mining_ram(df, 3, 0.1, 0.1)
     print(f_r3.head())
     print(ram_ar3.head())
 
-    #^ Mid-range phones
+    # Mid-range phones
     f_r0, ram_ar0 = assoc_mining_ram(df, 0, 0.1, 0.1)
     print(f_r0.head())
     print(ram_ar0.head())
 
-    #^ High-end phones
+    # High-end phones
     f_r1, ram_ar1 = assoc_mining_ram(df, 1, 0.1, 0.1)
     print(f_r1.head())
     print(ram_ar1.head())
 
-    #^ Very high-end phones (Luxury phones)
+    # Luxury phones
     f_r2, ram_ar2 = assoc_mining_ram(df, 2, 0.1, 0.1)
     print(f_r2.head())
     print(ram_ar2.head())
